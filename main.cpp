441acbedfdc9a51fe71b91868048590ca3e004e2
DeNecKit <deneckitc.2004@gmail.com>
2023-10-13 13:24:19 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,32 @@
+﻿#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Test project");
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        // ImGui code goes here
+
+        window.clear(sf::Color(36, 36, 36));
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    return 0;
+}
